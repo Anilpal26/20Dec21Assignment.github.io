e00f67499b68cd785d28aa43202729117e11e7ff
Anilpal26 <81358527+Anilpal26@users.noreply.github.com>
2021-12-29 13:08:33 +0530

    My 20 Dec 2021 Assignment

---
+++ 20 Dec 21/greaterThan.js
@@ -0,0 +1,9 @@
+//console.log("Its Working");
+
+var num = prompt("Enter the Number");
+    num = parseInt(num);
+if(num<10){
+  alert("Num is Less Than 10");
+}else{
+  alert("Num is Greater Than 10");
+}
---
+++ 20 Dec 21/highestVariable.js
@@ -0,0 +1,12 @@
+//console.log("Its Working");
+
+var a = 5;
+var b = 6;
+var c = 7
+if((a>b)&&(a>c)){
+  console.log("A is the Greatest Number");
+}else if((b>a)&&(b>c)){
+ console.log("B is the Greatest Number");
+}else if((c>a)&&(c>b)){
+ console.log("C is the Greatest Number");
+}
