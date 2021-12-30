ff9b18284107ea68208021ae8956ed2aa6a3ba4b
Anilpal26 <81358527+Anilpal26@users.noreply.github.com>
2021-12-30 15:11:57 +0530

    Add files via upload

---
+++ minimumNumber.js
@@ -0,0 +1,12 @@
+//console.log("Its Working");
+
+var a = 5;
+var b = 6;
+var c = 7
+if((a<b)&&(a<c)){
+  console.log("A is the Minimum Number");
+}else if((b<a)&&(b<c)){
+ console.log("B is the Minimum Number");
+}else if((c<a)&&(c<b)){
+ console.log("C is the Minimum Number");
+}
---
+++ vowel.js
@@ -0,0 +1,30 @@
+
+//Write a program to print wether the char is Vowel or Not.
+
+var char = prompt("Enter Character");
+
+if((char=="a")||(char=="A"))
+ {
+   alert("this is a Vowel");
+   console.log("this is a Vowel");
+ }else if((char=="e")||(char=="E"))
+ {
+   alert("this is a Vowel");
+   console.log("this is a Vowel");
+ }else if((char=="i")||(char=="I"))
+ {
+   alert("this is a Vowel");
+   console.log("this is a Vowel");
+ } else if((char=="o")||(char=="O"))
+ {
+   alert("this is a Vowel");
+   console.log("this is a Vowel");
+ }else if((char=="u")||(char=="U"))
+ {
+   alert("this is a Vowel");
+   console.log("this is a Vowel");
+ }else
+ {
+   alert("this charecter is a Consonent");
+   console.log("this is a Consonent");
+ }
