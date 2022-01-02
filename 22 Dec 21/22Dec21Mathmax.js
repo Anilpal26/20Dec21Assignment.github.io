fb57a6067ca6cac307f2aae443aa74106a8b9a36
Anilpal26 <81358527+Anilpal26@users.noreply.github.com>
2022-01-02 16:42:30 +0530

    22 Dec 21 Assignment

---
+++ 22 Dec 21/22Dec21Mathmax.js
@@ -0,0 +1,11 @@
+//console.log("Its Working");
+
+// Take the three number and find max(Using Array)
+
+/*var arr =['one','two','three'];
+arr.push('four');
+alert(arr);*/
+
+num-arr=[1,2,3,4,5,6,7,8];
+max-value=Math.max.apply(null,num-arr)
+console.log(max-value);
